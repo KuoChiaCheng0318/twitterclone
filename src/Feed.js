d01f953f9c8458d843fbde2e955c5ec0a92a7639
KuoChiaCheng0318 <82024833+KuoChiaCheng0318@users.noreply.github.com>
2023-01-14 22:57:32 -0700

    Feed Widget initialize
    video 1:35

--- src/Feed.js
+++ src/Feed.js
@@ -1,17 +1,17 @@
 import React from 'react'
+import './Feed.css'
 
 function Feed() {
   return (
-    <div>
+    <div className='feed'>
         {/* Header */}
-        <h2>Home</h2>
+        <div className='feed__header'>
+            <h2>Home</h2>
+        </div>
 
         {/* TweetBox */}
+        
 
-        {/* Post */}
-        {/* Post */}
-        {/* Post */}
-        {/* Post */}
         {/* Post */}
         
     </div>

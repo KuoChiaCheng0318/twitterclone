38096a54b28aaadc676ca846e400dc6f011ad997
KuoChiaCheng0318 <82024833+KuoChiaCheng0318@users.noreply.github.com>
2023-01-15 17:09:20 -0700

    add feed post animation
    video 3:25

--- src/Feed.js
+++ src/Feed.js
@@ -32,6 +32,7 @@ function Feed() {
         <FlipMove>
         {posts.map((post) =>(
           <Post 
+            key={post.text}
             displayName={post.displayName}
             username={post.username}
             verified={post.verified}
--- src/Post.js
+++ src/Post.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {forwardRef} from 'react'
 import "./Post.css"
 import { Avatar } from '@material-ui/core'
 import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
@@ -8,37 +8,39 @@ import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import PublishIcon from "@material-ui/icons/Publish";
 
 
-function Post( {displayName,username,verified,text,image,avatar} ) {
-  return (
-    <div className='post'>
-        <div className='post__avatar'>
-            <Avatar src="Sample_User_Icon.png" />
-        </div>
-        <div className='post__body'>
-            <div className='post__header'>
-                <div className='post__headerText'>
-                    <h3>{displayName} {" "}<span className="post__headerSpecial">
-                        {/* verified -> show verified button, if verified return false, don't show verified badge. */}
-                        {verified &&<VerifiedUserIcon className="post__badge" />}
-                        @{username}
-                    </span>
-                    </h3>
+const Post = forwardRef(
+    ({ displayName, username, verified, text, image, avatar }, ref) => {
+        return (
+            <div className='post' ref={ref}>
+                <div className='post__avatar'>
+                    <Avatar src={avatar} />
                 </div>
-                <div className='post__headerDescription'>
-                    <p>{text}</p>
+                <div className='post__body'>
+                    <div className='post__header'>
+                        <div className='post__headerText'>
+                            <h3>{displayName} {" "}<span className="post__headerSpecial">
+                                {/* verified -> show verified button, if verified return false, don't show verified badge. */}
+                                {verified &&<VerifiedUserIcon className="post__badge" />}
+                                @{username}
+                            </span>
+                            </h3>
+                        </div>
+                        <div className='post__headerDescription'>
+                            <p>{text}</p>
+                        </div>
+                    </div>
+                    <img src={image} alt=""/>
+                    {/* <img src="" alt=""/> */}
+                    <div className='post__footer'>
+                        <ChatBubbleOutlineIcon fontSize="small" />
+                        <RepeatIcon fontSize="small" />
+                        <FavoriteBorderIcon fontSize="small" />
+                        <PublishIcon fontSize="small" />
+                    </div>
                 </div>
             </div>
-            <img src={image} alt=""/>
-            {/* <img src="" alt=""/> */}
-            <div className='post__footer'>
-                <ChatBubbleOutlineIcon fontSize="small" />
-                <RepeatIcon fontSize="small" />
-                <FavoriteBorderIcon fontSize="small" />
-                <PublishIcon fontSize="small" />
-            </div>
-        </div>
-    </div>
-  )
-}
+        )
+    }
+)
 
 export default Post
